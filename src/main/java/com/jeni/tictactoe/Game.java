66e84960de5d3732ed1b70fc42bdb54dde33636c
Administrator <s7130457@gmail.com>
2018-04-25 14:10:07 +0800

    1. 增加平手的判斷 2. 將HomeController一些邏輯的部份抽到 Game.java裡

---
+++ src/main/java/com/jeni/tictactoe/Game.java
@@ -0,0 +1,68 @@
+package com.jeni.tictactoe;
+
+import javafx.util.Pair;
+
+import java.util.Arrays;
+
+public class Game {
+    private static Game game;
+    private String now = "X";
+    private static String[] player = new String[9];
+    private static final int[][] rule = {
+            {0, 1, 2},
+            {3, 4, 5},
+            {6, 7, 8},
+            {0, 3, 6},
+            {1, 4, 7},
+            {2, 5, 8},
+            {0, 4, 8},
+            {2, 4, 6}
+    };
+
+    public Game() {
+        initPlayer();
+    }
+    private void initPlayer() {
+        Arrays.fill(player,"");
+    }
+
+    public Pair getNextPlayer(int num) {
+        if (now == "O")
+            now = "X";
+        else
+            now = "O";
+        player[num] = now;
+        String win = checkWinner(player);
+        Pair result = new Pair(now, win);
+        return result;
+    }
+    public String checkWinner(String[] player) {
+        String winner = "";
+        for (int i = 0; i < rule.length; i++) {
+            int j;
+            for (j = 0; j < rule[i].length; j++) {
+                if (player[rule[i][j]] != now)
+                    break;
+            }
+            if (j == 3) {
+                if (now == "O")
+                    winner = "Winner: O";
+                else
+                    winner = "Winner: X";
+            }
+        }
+        int tmp;
+        for(tmp= 0 ; tmp < player.length ; tmp++) {
+            System.out.println("[tmp]="+tmp);
+            System.out.println("player[tmp]="+player[tmp]);
+            if(player[tmp] == "") {
+                break;
+            }
+        }
+        if(tmp == player.length) {
+            winner = "平手";
+        }
+        return winner;
+    }
+
+}
--- src/main/java/com/jeni/tictactoe/HomeController.java
+++ src/main/java/com/jeni/tictactoe/HomeController.java
@@ -7,66 +7,22 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
-    private String value = "X";
-    private static final int[][] lines = {
-            {0, 1, 2},
-            {3, 4, 5},
-            {6, 7, 8},
-            {0, 3, 6},
-            {1, 4, 7},
-            {2, 5, 8},
-            {0, 4, 8},
-            {2, 4, 6}
-    };
 
-    private static String[] player = new String[9];
-
+    private static  Game game = null;
     @RequestMapping("/")
     public String index() {
-        initPlayer();
+        game = new Game();
         return "index.html";
-    }
-
-    private void initPlayer() {
-        for (int i = 0; i < player.length; i++) {
-            player[i] = "";
-        }
     }
 
 
     @RequestMapping(value = "/ooxxRequest", method = RequestMethod.POST)
     @ResponseBody
     public Pair handler(@RequestParam("num") int num) {
-        if (value == "O")
-            value = "X";
-        else
-            value = "O";
-        player[num] = value;
-        String win = checkWinner(player);
-
-        Pair result = new Pair(value, win);
+        Pair result = game.getNextPlayer(num);
         return result;
     }
 
-    public String checkWinner(String[] player) {
-        String winner = "";
-        for (int i = 0; i < lines.length; i++) {
-            int j;
-            for (j = 0; j < lines[i].length; j++) {
-                if (player[lines[i][j]] != value)
-                    break;
-            }
-            if (j == 3) {
-                if (value == "O")
-                    winner = "O";
-                else
-                    winner = "X";
-            }
-        }
-
-        return winner;
-
-    }
 
 
 }
